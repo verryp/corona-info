c71541550ffdbe15cd2068a17e9b5d0cdab1cb26
verryp <verryp.dev@gmail.com>
2020-03-26 00:02:28 +0700

    first init

---
+++ src/pages/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <h1>
+      {{ items }}
+    </h1>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+
+  data() {
+    return {
+      items: {}
+    }
+  },
+
+  async mounted() {
+    try {
+      const result = await axios.get(`https://appku19.herokuapp.com/indonesia`)
+      
+      this.items = result.data.result[0].country
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
